585b5550567725dbf74ebfd992045b1a79f9262a
naufalg <naufalghifari7@gmail.com>
2020-08-26 00:25:50 +0700

    add scrollrevealjs to home and portofolio pages, temp remove materialize hamburger

--- js/home.js
+++ js/home.js
@@ -1,3 +1,18 @@
-ScrollReveal({reset: true}).reveal('.jumbotron');
-ScrollReveal({reset: true}).reveal('.jumbotron-fluid');
-ScrollReveal({reset: true}).reveal('#stack');
+// scroll reveal js
+// Jumbotron
+ScrollReveal().reveal('.jumbotron', {easing:"ease-out"});
+ScrollReveal().reveal('#together', {delay:800, distance:"200px", easing:"ease-out", origin:"right"});
+ScrollReveal().reveal('#lets', {delay:1400, distance:"200px", easing:"ease-out", origin:"right"});
+ScrollReveal().reveal('#to', {delay:2800, distance:"200px", easing:"ease-out", origin:"right"});
+ScrollReveal().reveal('#life', {delay:3500, distance:"200px", easing:"ease-out", origin:"right"});
+ScrollReveal().reveal('#learn-more', {delay:5000, distance:"200px", easing:"ease-out", origin:"right"});
+// about
+ScrollReveal().reveal('.avatar', {delay:200, distance:"200px", easing:"ease-out", origin:"left"});
+ScrollReveal().reveal('#abt-txt-1', {delay:300, distance:"200px", easing:"ease-out", origin:"right"});
+ScrollReveal().reveal('#abt-txt-2', {delay:400, distance:"200px", easing:"ease-out", origin:"right"});
+ScrollReveal().reveal('#abt-txt-3', {delay:500, distance:"200px", easing:"ease-out", origin:"bottom"});
+// stack
+ScrollReveal({reset: true}).reveal('#stack-1', {delay:200, distance: "200px", easing: "ease-in"});
+ScrollReveal({reset: true}).reveal('#stack-2', {delay:400, distance: "200px", easing: "ease-in"});
+ScrollReveal({reset: true}).reveal('#stack-3', {delay:600, distance: "200px", easing: "ease-in"});
+ScrollReveal({reset: true}).reveal('#stack-4', {delay:800, distance: "200px", easing: "ease-in"});
--- pages/portfolio/js/portfolio.js
+++ pages/portfolio/js/portfolio.js
@@ -1,3 +1,21 @@
+// materialize sidenav
 $(document).ready(function(){
     $('.sidenav').sidenav();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+
+//   scrollrevealjs
+// headline
+ScrollReveal().reveal('.avatar', {distance: "200px", easing: "ease-out"});
+ScrollReveal().reveal('#hi', {delay:500, distance: "200px", easing: "ease-out"});
+ScrollReveal().reveal('#i-am', {delay:1200, distance: "200px", easing: "ease-out"});
+ScrollReveal().reveal('#here', {delay:1600, distance: "200px", easing: "ease-out"});
+// works
+ScrollReveal().reveal('#card-1', {delay:2000, interval:400, distance: "200px", easing: "ease-out"});
+ScrollReveal().reveal('#card-2', {delay:2200, interval:400, distance: "200px", easing: "ease-out"});
+ScrollReveal().reveal('#card-3', {delay:2400, interval:400, distance: "200px", easing: "ease-out"});
