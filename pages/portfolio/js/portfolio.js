aeb7c70c594b5d3b8779e9be471477a7539301e0
naufalg <naufalghifari7@gmail.com>
2020-09-12 21:41:29 +0700

    add new project in portofolio page

--- pages/portfolio/js/portfolio.js
+++ pages/portfolio/js/portfolio.js
@@ -15,7 +15,9 @@ ScrollReveal().reveal('.avatar', {distance: "200px", easing: "ease-out"});
 ScrollReveal().reveal('#hi', {delay:400, distance: "200px", easing: "ease-out"});
 ScrollReveal().reveal('#i-am', {delay:1000, distance: "200px", easing: "ease-out"});
 ScrollReveal().reveal('#here', {delay:1400, distance: "200px", easing: "ease-out"});
+ScrollReveal().reveal('#here', {delay:1400, distance: "200px", easing: "ease-out"});
+ScrollReveal().reveal('.content-break', {delay:1400, distance: "200px", easing: "ease-out"});
 // works
-ScrollReveal().reveal('#card-1', {delay:1500, interval:600, distance: "200px", easing: "ease-out"});
+// ScrollReveal().reveal('#card-1', {delay:1500, interval:600, distance: "200px", easing: "ease-out"});
 // ScrollReveal().reveal('#card-2', {delay:2200, interval:400, distance: "200px", easing: "ease-out"});
 // ScrollReveal().reveal('#card-3', {delay:2400, interval:400, distance: "200px", easing: "ease-out"});
